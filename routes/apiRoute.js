7ef972cf4bcf712e8a61ee58850d8cd8bf7f9568
Kgiunta <giunta.kyle@yahoo.com>
2022-04-14 18:53:31 -0400

    pretutor session

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
+// const noteData = require("../db/db.json");
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
 
 // '/api/notes/
-router.get("/notes", (req, res) => res.json(noteData));
+router.get("/notes", async (req, res) => {
+  const noteData = await readFileAsync("db/db.json", "utf8");
+  console.log(typeof noteData);
+  res.json(JSON.parse(noteData));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const path = require("path");
 const noteData = require("./db/db.json");
 const routehtml = require("./routes/htmlRoute");
 const routeApi = require("./routes/apiRoute");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const id = require("./helper/id");
-const { title } = require("process");
-const { text } = require("express");
-const { fstat } = require("fs");
 
 const app = express();
 
@@ -19,58 +16,56 @@ app.use(express.static("public"));
 app.use("/api", routeApi);
 app.use("/", routehtml);
 
-// const { textNote, titleNote } = req.body;
+// app.post("/notes", (req, res) => {
+//   // Log that a POST request was received
+//   console.info(`${req.method} request received to add a review`);
 
-// // If all the required properties are present
-// if (textNote && titleNote) {
-//   // Variable for the object we will save
-//   const newNote = {
-//     textNote,
-//     titleNote,
-//     note_id: id(),
-//   };
+//   // Destructuring assignment for the items in req.body
+//   const { text, title } = req.body;
 
-//   // Obtain existing reviews
-//   fs.readFile("./db/db.json", "utf8", (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       // Convert string into JSON object
-//       const parsedNotes = JSON.parse(data);
+//   // If all the required properties are present
+//   if (text && title) {
+//     // Variable for the object we will save
+//     const newtask = {
+//       text,
+//       title,
+//       review_id: id(),
+//     };
 
-//       // Add a new review
-//       parsedNotes.push(newNote);
+//     // Obtain existing reviews
+//     fs.readFile("./db/db.json", "utf8", (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         // Convert string into JSON object
+//         const parsedNotes = JSON.parse(data);
 
-//       // Write updated reviews back to the file
-//       fs.writeFile(
-//         "./db/db.json",
-//         JSON.stringify(parsedNotes, null, 4),
-//         (writeErr) =>
-//           writeErr
-//             ? console.error(writeErr)
-//             : console.info("Successfully updated note!")
-//       );
-//     }
-//   });
-//   const response = {
-//     status: "success",
-//     body: newNote,
-//   };
+//         // Add a new review
+//         parsedNotes.push(newtask);
 
-//   console.log(response);
-//   res.status(201).json(response);
-// } else {
-//   res.status(500).json("Error in posting");
-// }
+//         // Write updated reviews back to the file
+//         fs.writeFile(
+//           "./db/db.json",
+//           JSON.stringify(parsedNotes, null, 4),
+//           (writeErr) =>
+//             writeErr
+//               ? console.error(writeErr)
+//               : console.info("Successfully updated reviews!")
+//         );
+//       }
+//     });
 
-// if (title && text) {
-//   const newNote = {
-//     title,
-//     text,
-//     noteid: id(),
-//   };
-//   fs.readFile("./db/db.json","")
-// }
+//     const response = {
+//       status: "success",
+//       body: newtask,
+//     };
+
+//     console.log(response);
+//     res.status(201).json(response);
+//   } else {
+//     res.status(500).json("Error in posting review");
+//   }
+// });
 
 // app.get("/notes", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -80,8 +75,6 @@ app.use("/", routehtml);
 //   res.sendFile(path.join(__dirname, "index.html"));
 // });
 // res.json(`${req.method} request received`);
-
-app.get("/api/notes", (req, res) => res.json(noteData));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
